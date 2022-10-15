d4ef1ea0fd78fc6f6577969415944933cd81d3fa
Arnau González <arnau.gonzalez@skyscanner.net>
2022-10-15 17:49:58 +0200

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-const contractAddress = '0x094323315830396042a38b82dab8d783a0de928f';
-const presaleDate = 'September 24th';
-const publicMintDate = 'October 1st';
+const contractAddress = '0x165B5e297025c81EDe46F5452dd835A594F09c65';
+const presaleDate = 'October 22nd';
+const publicMintDate = 'October 24th';
 
 export { contractAddress, presaleDate, publicMintDate };
